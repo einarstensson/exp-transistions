fef6336d81f6a7ff50860814f2bb7716355a5b7e
einarstensson <einar.stensson@gmail.com>
2016-08-18 09:36:31 -0400

    Add changes

---
+++ animate.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    /* This adds the overlay-inc class to the element, which triggers the
+    animations. */
+    $("#change").on('click', function(){
+        $(".overlay").addClass("overlay-inc");
+    });
+
+});
